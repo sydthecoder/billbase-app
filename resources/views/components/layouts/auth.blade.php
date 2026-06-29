1dcfc358398f0615bcabff83a6033aa657eea8e8
Sydney Munasi <sydneylutendo5@gmail.com>
2026-06-29 20:36:17 +0200

    Remove tailwind, make v1 of sidebar

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'BillBase' }}</title>
-    @vite('resources/css/app.css')
+    @vite('resources/css/main.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
